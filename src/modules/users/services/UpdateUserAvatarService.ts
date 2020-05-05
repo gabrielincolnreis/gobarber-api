65d5afab431e6860f44070a33dd603951c42c746
Matheus Castro <matheuscastroweb@gmail.com>
2020-05-05 19:31:47 -0300

    Module tests completed by the user in the application

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -4,6 +4,7 @@ import uploadConfig from '@config/upload';
 import path from 'path';
 import fs from 'fs';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import { inject, injectable } from 'tsyringe';
 
 interface IRequest {
@@ -15,6 +16,9 @@ class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
 
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
@@ -25,15 +29,13 @@ class UpdateUserAvatarService {
     }
 
     if (user.avatar) {
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
-      }
+      await this.storageProvider.deleteFile(user.avatar);
     }
 
-    user.avatar = avatarFilename;
+    const filename = await this.storageProvider.saveFile(avatarFilename);
+
+    user.avatar = filename;
+
     await this.usersRepository.save(user);
 
     return user;
---
+++ src/shared/container/providers/StorageProvider/implementations/DiskStorageProvider.ts
@@ -0,0 +1,28 @@
+import IStorageProvider from '../models/IStorageProvider';
+import fs from 'fs';
+import path from 'path';
+import uploadConfig from '@config/upload';
+class DiskStorageProvider implements IStorageProvider {
+  public async saveFile(file: string): Promise<string> {
+    await fs.promises.rename(
+      path.resolve(uploadConfig.tempFolder, file),
+      path.resolve(uploadConfig.uploadsFolder, file),
+    );
+
+    return file;
+  }
+
+  public async deleteFile(file: string): Promise<void> {
+    const filePath = path.resolve(uploadConfig.uploadsFolder, file);
+
+    try {
+      await fs.promises.stat(filePath);
+    } catch {
+      return;
+    }
+
+    fs.promises.unlink(filePath);
+  }
+}
+
+export default DiskStorageProvider;
