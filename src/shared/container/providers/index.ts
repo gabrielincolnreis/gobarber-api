6e462247fcfc2b5cc141c9f216d4f942ffe5cd22
Matheus Castro <matheuscastroweb@gmail.com>
2020-05-14 16:40:19 -0300

    Created module notifications

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,8 +1,14 @@
 import { container } from 'tsyringe';
+
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import NotificationsRepository from '@modules/notifications/infra/typeorm/repositories/NotificationsRepository';
+
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
+
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtheralMailProvider from './MailProvider/implementations/EtherealMailProvider';
+
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsTemplateMailProvider from './MailTemplateProvider/implementations/HandlebarsTemplateMailProvider';
 
@@ -19,4 +25,9 @@ container.registerSingleton<IMailTemplateProvider>(
 container.registerInstance<IMailProvider>(
   'MailProvider',
   container.resolve(EtheralMailProvider),
+);
+
+container.registerInstance<INotificationsRepository>(
+  'NotificationsRepository',
+  container.resolve(NotificationsRepository),
 );
