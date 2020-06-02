28f4b14c8bae83037bb2bf3109a8fcb3218fca0e
Matheus Castro <matheuscastroweb@gmail.com>
2020-06-02 16:41:28 -0300

    Added S3 AWS Storage

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,20 +1,44 @@
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import crypto from 'crypto';
 import path from 'path';
 
 const tempFolder = path.resolve(__dirname, '..', '..', 'temp');
 const uploadsFolder = path.resolve(tempFolder, 'uploads');
 
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+  tempFolder: string;
+  uploadsFolder: string;
+  multer: {
+    storage: StorageEngine;
+  };
+
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
 export default {
+  driver: process.env.STORAGE_DRIVER,
   tempFolder,
   uploadsFolder,
-  storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'temp'),
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('HEX');
-      const fileName = `${fileHash}-${file.originalname}`;
+  multer: {
+    storage: multer.diskStorage({
+      destination: path.resolve(__dirname, '..', '..', 'temp'),
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('HEX');
+        const fileName = `${fileHash}-${file.originalname}`;
 
-      return callback(null, fileName);
+        return callback(null, fileName);
+      },
+    }),
+  },
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'app-bucket',
     },
-  }),
-};
+  },
+} as IUploadConfig;
--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -9,7 +9,7 @@ import UserAvatarController from '../controllers/UserAvatarController';
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
 const usersRouter = Router();
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 
 usersRouter.post(
   '/',
