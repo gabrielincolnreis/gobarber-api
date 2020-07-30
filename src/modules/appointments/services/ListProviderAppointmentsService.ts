97b399247d88657a0934345fbec33e5a49e4fbd3
Gabriellincoln2 <gabrielincoln17@gmail.com>
2020-07-30 11:00:59 -0300

    att

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -30,10 +30,11 @@ class ListProviderAppointmentsService {
   }: IRequest): Promise<Appointments[]> {
     const cacheKey = `provider-appointments:${provider_id}${year}-${month}-${day}`;
 
-    let appointments = await this.cacheProvider.recover<Appointments[]>(
-      cacheKey,
-    );
+    // let appointments = await this.cacheProvider.recover<Appointments[]>(
+    //   cacheKey,
+    // );
 
+    let appointments = null;
     if (!appointments) {
       appointments = await this.appointmentsRepository.findAllInDayFromProvider(
         {
